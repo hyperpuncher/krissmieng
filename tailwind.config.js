f18c2b41a858ace3b661b7bbe53b57c94ee97f27
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2024-04-18 12:45:37 +0300

    animation test

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,25 @@ module.exports = {
 					"0%, 50%": { transform: "rotate(-10deg)" },
 					"25%, 75%": { transform: "rotate(10deg)" },
 				},
+				zoom: {
+					"0%": { transform: "scale(1)" },
+					"50%": { transform: "scale(1.1)" },
+				},
+				"slide-chat": {
+					to: {
+						opacity: "1",
+						transform: "translateX(0)",
+					},
+				},
+				scroll: {
+					to: { transform: "translateX(-100%)" },
+				},
 			},
 			animation: {
 				wiggle: "wiggle 1s ease-in-out",
+				zoom: "scale linear",
+				"slide-chat": "slide-chat ease forwards",
+				scroll: "scroll linear forwards",
 			},
 		},
 	},
