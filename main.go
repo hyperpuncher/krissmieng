a9c44e7e9bb2692189f8bdec056cab2ffc9ba04a
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2024-04-19 08:45:31 +0300

    add live server func

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/a-h/templ"
 	"github.com/hyperpuncher/krissmieng/components"
 )
 
 func main() {
+	serveHtml()
+}
+
+func createHtml() {
 	f, err := os.Create("index.html")
 	if err != nil {
 		log.Fatalf("failed to create output file: %v", err)
@@ -18,4 +25,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to write output file: %v", err)
 	}
+}
+
+func serveHtml() {
+	http.Handle("/", templ.Handler(components.Page()))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	fmt.Println("serving on http://localhost:6969")
+	http.ListenAndServe(":6969", nil)
 }
